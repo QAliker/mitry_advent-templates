79a6e164097343d24a446dd55767460becdac2d6
Quentin <alikerquentin0@gmail.com>
2023-03-28 17:47:18 +0200

    calendar update

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -16,20 +16,41 @@ document.addEventListener('DOMContentLoaded', function() {
       locale: 'fr',
       events: [
         {
-          title: 'EDS et Culte',
+          title: 'Etude de la bible',
           daysOfWeek: '6',
           startTime: '09:45:00',
-          endTime: '12:30:00',
+          endTime: '10:50:00',
           backgroundColor: '#153e52',
           borderColor: '#153e52',
           textColor: '#fff',
         },
+        {
+          title: 'Culte',
+          daysOfWeek: '6',
+          startTime: '11:15:00',
+          endTime: '12:30:00',
+          backgroundColor: '#14bf98',
+          borderColor: '#14bf98',
+          textColor: '#fff',
+        },
+        {
+          title: 'Réunion de prière',
+          daysOfWeek: '2',
+          startTime: '19:00:00',
+          endTime: '20:00:00',
+          backgroundColor: '#75751c',
+          borderColor: '#75751c',
+          textColor: '#fff',
+          description: "Sur Zoom"
+        },
+      
       ],
       
 
     });
     calendar.render();
   });
+
 
 
       
